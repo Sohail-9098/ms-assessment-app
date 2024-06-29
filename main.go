5bec5c162436feef2ac5893ab27834c01bfa0fe2
Sohail Khan <sohail.a.khan@ericsson.com>
2024-06-29 12:40:01 +0200

    more questions
    Change-Id: I2cac4da204aab90fc41e1cafeb031b3f30abbe86

--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 	"github.com/Sohail-9098/ms-assessment-app/model/assessment"
 	"github.com/Sohail-9098/ms-assessment-app/model/question"
 	"github.com/Sohail-9098/ms-assessment-app/model/result"
+	"github.com/Sohail-9098/ms-assessment-app/model/user"
 )
 
 func main() {
 	fmt.Println("Welcome to Assessment App!")
+	newUser := user.User{UserId: 1, UserName: "Tarannum Ali"}
 	newAssessment := assessment.Assessment{Questions: question.QuestionSet, TimeLimit: 30}
 	userResult := result.Result{Assessment: newAssessment}
 	for _, question := range newAssessment.Questions {
@@ -26,7 +28,9 @@ func main() {
 			userResult.MarksObtained -= 1
 		}
 	}
+	newUser.TestResult = append(newUser.TestResult, userResult)
 	fmt.Println("Your Result: ")
+	fmt.Printf("User Name: %v, User Score: %v\n", newUser.UserName, newUser.TestResult[0].MarksObtained)
 	fmt.Printf("%d/%d\n", userResult.MarksObtained, userResult.MarksTotal)
 	fmt.Printf("%v percent \n", userResult.MarksObtained*100/userResult.MarksTotal)
 }
