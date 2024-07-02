00129a7a5acdd023a0ee2d20a875b0a6bc23a7e0
Sohail Khan <sohail.a.khan@ericsson.com>
2024-07-02 13:38:42 +0200

    remove unused code
    Change-Id: Ife4be3cac369da9e74ea07ba512e9e4734a050f5

--- cmd/main.go
+++ cmd/main.go
@@ -1,55 +1,8 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/Sohail-9098/ms-assessment-app/internal/db"
-	"github.com/Sohail-9098/ms-assessment-app/internal/result"
 	"github.com/Sohail-9098/ms-assessment-app/internal/server"
 )
-
-func SomeFunction() {
-	fmt.Println("Welcome to Assessment App!")
-	// User LogsIn
-	newUser := db.GetUserById(1)
-
-	// Creates Assessment and save it to db
-	// newAssessment := assessment.NewAssessment([]question.MultipleChoiceQuestion{db.GetMCQById(1), db.GetMCQById(2), db.GetMCQById(3)})
-	// db.AddAssessment(newAssessment)
-
-	// Fetch Assessment
-	assessmentToTake := db.GetAssessmentById(1)
-
-	// Create User Result
-	assessmentResult := result.NewResult(assessmentToTake)
-
-	// Start the assessment
-	for _, question := range assessmentToTake.Questions {
-		// Ask Question
-		fmt.Println(question.QuestionText)
-
-		for i, option := range question.Options {
-			fmt.Printf("%d: %v\n", i+1, option)
-		}
-		// Get user answer
-		var answerIndex int
-		fmt.Scanln(&answerIndex)
-		selectedAnswer := question.Options[answerIndex-1]
-		assessmentResult.AssessAnswer(question, selectedAnswer)
-	}
-
-	// update the db with results
-	db.AddUserResult(newUser, assessmentResult)
-
-	db.GetUserResults(&newUser)
-
-	// Print results
-	fmt.Println("Your Results: ")
-	for _, result := range newUser.TestResult {
-		fmt.Printf("User Name: %v, Correct: %v, Incorrect: %v\n", newUser.UserName, result.Correct, result.Incorrect)
-		fmt.Printf("Total Score: %d/%d\n", result.MarksObtained, result.MarksTotal)
-	}
-}
 
 func main() {
 	server.StartServer()
